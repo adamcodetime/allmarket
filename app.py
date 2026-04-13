373c45e2306da025f25e602ad53dddfa4f83f2f6
ADAMthesmarty <adam.towfek@outlook.com>
2026-04-13 07:54:29 +0400

    update

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
+
 def get_db():
-    conn = sqlite3.connect("db.sqlite3")
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
